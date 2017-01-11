df37298e68cfe2012b916bc612b906e086b19afc
woshipm <wxz_work@163.com>
2017-01-11 11:13:31 +0800

    Initial Commit

---
+++ 毫秒倒计时/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  毫秒倒计时
+//
+//  Created by WOSHIPM on 17/1/11.
+//  Copyright © 2017年 WOSHIPM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 毫秒倒计时/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  毫秒倒计时
+//
+//  Created by WOSHIPM on 17/1/11.
+//  Copyright © 2017年 WOSHIPM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
